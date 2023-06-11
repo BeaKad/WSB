ab7921e34a4f64712fed46662f26c0854c9b2395
BeaKad <bozalek.beata@gmail.com>
2023-06-11 21:55:02 +0200

    logowanie i rejestracja

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('zal.urls')),
+    path('',include('users.urls'))
 ]
